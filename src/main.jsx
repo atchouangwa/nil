2b9925f623090dfe98ce02427f4c1b4d6c807d63
Alex Tchouangwa <49045243+atchouangwa@users.noreply.github.com>
2026-09-15 14:54:30 -0500

    Load vertical landing page design styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './components/LandingPageV2.css'
 import './components/ConfirmedVideoLibrary.css'
 import './components/LandingVsl.css'
 import App from './App'
