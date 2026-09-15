74ab7fec3d823fa2e65cb6b0929c90ffab63d88c
Alex Tchouangwa <49045243+atchouangwa@users.noreply.github.com>
2026-09-15 14:53:19 -0500

    Load landing VSL styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './components/ConfirmedVideoLibrary.css'
+import './components/LandingVsl.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
